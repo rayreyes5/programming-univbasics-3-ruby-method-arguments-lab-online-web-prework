a908999693e53554f874339e13d9c17a451f5763
Ray Reyes <rayreyes5@gmail.com>
2019-09-02 09:15:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 `#introduction  `name` `"Hi, my name is #{name}."`
-#introduction_with_language name language  "Hi, my name is #{name} and I am learning to program in #{language}."
+#introduction_with_language name language  "Hi, my name is #{name} and I am learning to program in #{Ruby}."
 #introduction_with_language_optional name
 
 
